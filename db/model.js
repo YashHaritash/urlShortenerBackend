dbb4fa1561b989f8ca5a72b83e4f3376a8dcefac
YashHaritash <yashharitash4@gmail.com>
2024-06-29 18:01:55 +0530

    env vars

--- db/model.js
+++ db/model.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('urlshortdb', 'urluser', 'urlpass', {
-    host: 'localhost',
-    dialect: 'mysql'
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
 });
 
 const Urls = db.define('url', {
